15d9f157dd867627fa3d9c61d33ac43c58220a81
Laura Gonzalez Fernandez <l.gonzalezfernand@alumnos.urjc.es>
2020-05-05 00:21:54 +0200

    Add models for app

--- miscosas/models.py
+++ miscosas/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+"""
+Models for app MisCosas
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Feed(models.Model):
+    key = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    provider = models.CharField(max_length=32)
+    user = models.ForeignKey(User, models.PROTECT)
+
+    def __str__(self):
+        return self.provider + ': ' + self.title
+
+class Item(models.Model):
+    key = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    feed = models.ForeignKey(Feed, models.CASCADE)
+    upvotes = models.ManyToManyField(User, related_name="upvotes")
+    downvotes = models.ManyToManyField(User, related_name="downvotes")
+    description = models.TextField(blank=True, default='')
+    picture = models.URLField(blank=True, default='')
+
+    def __str__(self):
+        return str(self.feed) + ", " + self.title
+
+class Comment(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.CharField(max_length=256)
+    date = models.DateTimeField(auto_now=True)
+    item = models.ForeignKey(Item, models.CASCADE)
+    user = models.ForeignKey(User, models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+class UserData(models.Model):
+    picture = models.URLField()
+    django_user = models.OneToOneField(User, models.PROTECT)
+
+    def __str__(self):
+        return str(self.django_user)
